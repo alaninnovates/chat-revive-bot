cfc26fedd1f133066a3810212717322e124b4ee2
Alan <40917647+alanlichen@users.noreply.github.com>
2022-08-29 22:58:42 +0000

    Edit timing logic

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
 
 client.once('ready', (c) => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+const MINUTE = 60 * 1000;
+const DEFAULT_WAIT_TIME = 30 * MINUTE;
+
 let lastMessageTime = new Date();
+let pingCount = 0;
+let waitTime = DEFAULT_WAIT_TIME;
+
 client.on('messageCreate', (message) => {
     if (message.channel.id !== process.env.CHANNEL_ID) return;
     lastMessageTime = new Date();
+    if (message.author.id === client.user.id) return;
+    pingCount = 0;
+    waitTime = DEFAULT_WAIT_TIME;
 });
 
-const TWENTY_MINUTES_MS = 20 * 60 * 1000;
-
 setInterval(async () => {
-    if (Date.now() < lastMessageTime.getTime() + TWENTY_MINUTES_MS) return;
-    console.log('We pingin them >:)');
+    if (pingCount === 3 || Date.now() < lastMessageTime.getTime() + waitTime) return;
     const channel = await client.channels.fetch(process.env.CHANNEL_ID)
-    channel.send(`<@&${process.env.ROLE_ID}>`);
-    lastMessageTime = new Date();
+    channel.send(`<@&${process.env.ROLE_ID}>\nIt's been ${waitTime/MINUTE} minutes since I have last seen someone talk.`);
+    pingCount++;
+    waitTime *= 2;
 }, 1000);
 
 client.login(process.env.DISCORD_TOKEN);
