6408696f838bf22a19e1ba6f30970ea830ccff4b
Alan <40917647+alanlichen@users.noreply.github.com>
2022-08-30 03:38:15 +0000

    Improve logging

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
+const log = (...args) => {
+    console.log(`[LOG] [${new Date().toUTCString()}]`, ...args);
+}
+
 client.once('ready', (c) => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+    log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 const MINUTE = 60 * 1000;
@@ -19,16 +22,21 @@ client.on('messageCreate', (message) => {
     if (message.channel.id !== process.env.CHANNEL_ID) return;
     lastMessageTime = new Date();
     if (message.author.id === client.user.id) return;
+    log('Resetting pingCount and waitTime');
     pingCount = 0;
     waitTime = DEFAULT_WAIT_TIME;
 });
 
 setInterval(async () => {
-    if (pingCount === 3 || Date.now() < lastMessageTime.getTime() + waitTime) return;
+    if (Date.now() < lastMessageTime.getTime() + waitTime) return;
+    if (pingCount === 3) {
+        return log('pingCount is 3');
+    }
     const channel = await client.channels.fetch(process.env.CHANNEL_ID)
     channel.send(`<@&${process.env.ROLE_ID}>\nIt's been ${waitTime/MINUTE} minutes since I have last seen someone talk.`);
     pingCount++;
     waitTime *= 2;
+    log(`Pinging... waitTime is now: ${waitTime} and pingCount is now ${pingCount}`);
 }, 1000);
 
 client.login(process.env.DISCORD_TOKEN);
